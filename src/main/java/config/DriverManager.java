6c7a67383e7136820f0710efe8e17167810c2736
codeforcoders-420 <javacodeforlife420@gmail.com>
2025-01-29 16:36:54 -0500

    Update DriverManager.java

--- src/main/java/config/DriverManager.java
+++ src/main/java/config/DriverManager.java
@@ -3,14 +3,20 @@ package config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void initializeDriver() {
+        // Automatically detect and download the compatible ChromeDriver version
         WebDriverManager.chromedriver().setup();
-        driver.set(new ChromeDriver());
-        getDriver().manage().window().maximize();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized"); // Open browser in maximized mode
+        options.addArguments("--disable-notifications"); // Disable notifications
+        
+        driver.set(new ChromeDriver(options));
     }
 
     public static WebDriver getDriver() {
